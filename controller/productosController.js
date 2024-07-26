952e984047a936c39e050c2b2b40dd253aa21b83
DanielaPonce25 <danielapringlemorgan@gmail.com>
2024-07-25 23:33:53 -0300

    crear producto funciona

--- controller/productosController.js
+++ controller/productosController.js
@@ -8,7 +8,7 @@ const productosController = {
     list: (req, res) =>{
         res.render('producto', {productos});
     },
-    
+
     create: (req, res) =>{
         res.render('productos/creacionProd');
     },
@@ -19,24 +19,52 @@ const productosController = {
         const precio = req.body.precio;
 
         const nuevoProducto = {
-            id: productos.length + 1,
+        id: productos.length + 1,
             marca:marca,
             descripcion:descripcion,
-            precio:precio
+           precio:precio
         };
-        
+
         try{
-            productos.push(nuevoProducto);
-            fs.writeFileSync(productosFilePath, JSON.stringify(productos, null, " "));
-            res.redirect('/');
+          productos.push(nuevoProducto);
+          fs.writeFileSync(productosFilePath, JSON.stringify(productos, null, " "));
+          res.redirect('/');
             }
             catch(error){
-                console.log("Error al guardar el producto");
-                console.error(error)
-                res.status(500).send("Error al guardar el producto en el servidor")
+              console.log("Error al guardar el producto");
+              console.error(error)
+              res.status(500).send("Error al guardar el producto en el servidor")
             }
     },
 
+    edit: (req, res) => {
+        const id = req.params.id;
+        const producto = productos.find(producto => producto.id == id);
+        res.render('productos/editarProd', {producto});
+    },
+
+//     update: (req, res) => {
+//         const id = req.params.id;
+//         const marca = req.body.marca;
+//         const descripcion = req.body.descripcion;
+//         const precio = req.body.precio;
+
+//         const productoUpdate = productos.findIndex(producto => producto.id == id);
+//         if(productoUpdate !== -1){
+//             productos[productoUpdate] = { id: Number(id), marca, descripcion, precio};
+//             try{
+//                 fs.writeFileSync(productosFilePath, JSON.stringify(productos, null, " "));
+//                 res.redirect('/');
+//                 }
+//                 catch(error){
+//                     console.log("Error al guardar el producto");
+//                     console.error(error)
+//                     res.status(500).send("Error al guardar el producto en el servidor")
+//         }
+//     } else{
+//         res.status(404).send("Producto no encontrado");
+//     }
+// }
 }
 
 module.exports = productosController;
