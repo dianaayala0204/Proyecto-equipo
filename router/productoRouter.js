269ddb59d73b5c0c3cc864a61457dda801f2e170
dianaayala0204 <ayala.diana.0204@gmail.com>
2024-08-04 22:37:22 -0600

    Version 15 anñadir funciones de producto y agregar route de producto en productoRouter.js

--- router/productoRouter.js
+++ router/productoRouter.js
@@ -30,21 +30,30 @@ router.get('/', (req, res) => {
 });
 
 router.get('/promociones', (req, res) => {
-     res.render('promociones');
+    res.render('promociones');
 });
 
-router.get('/producto', (req, res) => {
-     res.render('producto');
-});
+/** Se modificaron las routes
+ * router get se quita el res.render porque la vista /producto necesita una lista de productos
+ * se cambia la route raiz / porque debe direccionar a home
+ * se comentan las siguiente lineas de código porque router get producto está incompleto.
+ */
 
+// router.get('/producto', (req, res) => {
+//      res.render('producto', { productos });
+// });
 
-router.get("/", productoController.list);
+/** Se cambio la route / porque ya existe
+ * Se muestra a traves de productoController método list
+ * Este es el render completo con la lista de productos, extraída de productos.json
+ */
+router.get("/producto", productoController.list);
 
 router.get("/create", productoController.create);
 
 router.post("/create", upload.single('imagen'), productoController.stock);
 
-// MODIFICADO DIANA
+
 
 // Traer los datos del servidor al navegador 
 router.get("/:id/edit", productoController.edit);
@@ -54,5 +63,12 @@ router.put("/:id", upload.single('imagen'), productoController.update);
 router.get("/:id/delete", productoController.delete);
 // router.get("/eliminar:id", productoController.delete);
 router.delete("/:id", productoController.destroy);
+
+/** Añadir router tipo get productos nuevos para iframe productos nuevos. 
+router.get("/productos-nuevos", productoController.productos_nuevos);
+*/
+/** Añadir router tipo get promociones para promociones 
+router.get("/promociones", productoController.promociones);
+*/
 
 module.exports = router;
